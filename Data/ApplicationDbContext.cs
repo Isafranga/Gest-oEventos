95e6cdfb35e3b022f25e896e1e6ac63cc942dab4
Isafranga <isadora.a.oliveira6@aluno.senai.br>
2026-03-13 11:42:18 -0300

    Adicionar arquivos de projeto.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using GestãoEventos.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestãoEventos.Data;
+public class ApplicationDbContext: IdentityDbContext 
+{
+
+    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+
+    public DbSet<Categoria> Categorias { get; set; }
+
+    public DbSet<Local> Locais { get; set; }
+}
---
+++ Data/Migrations/20260313141603_AddLocalCorrect.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestãoEventos.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLocalCorrect : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Local",
+                table: "Local");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categoria",
+                table: "Categoria");
+
+            migrationBuilder.RenameTable(
+                name: "Local",
+                newName: "Locais");
+
+            migrationBuilder.RenameTable(
+                name: "Categoria",
+                newName: "Categorias");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Locais",
+                table: "Locais",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categorias",
+                table: "Categorias",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Locais",
+                table: "Locais");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categorias",
+                table: "Categorias");
+
+            migrationBuilder.RenameTable(
+                name: "Locais",
+                newName: "Local");
+
+            migrationBuilder.RenameTable(
+                name: "Categorias",
+                newName: "Categoria");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Local",
+                table: "Local",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categoria",
+                table: "Categoria",
+                column: "Id");
+        }
+    }
+}
